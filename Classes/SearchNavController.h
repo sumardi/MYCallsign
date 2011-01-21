1adb52f2ff1531600ceb6eb7077fda5e885e8c95
smd <me@sumardi.net>
2011-01-22 02:13:54 +0800

    Modify credits & copyrights.

--- Classes/SearchNavController.h
+++ Classes/SearchNavController.h
@@ -1,10 +1,20 @@
-//
-//  SearchNavController.h
-//  MYCallsign
-//
-//  Created by Sumardi Shukor on 1/20/11.
-//  Copyright 2011 Software Machine Development. All rights reserved.
-//
+// MYCallsign - The Malaysian Amateur Radio iPhone app.
+// 
+// Copyright (C) 2010  Software Machine Development <support@smd.com.my> - 
+// Sumardi Shukor <me@sumardi.net>
+// 
+// MYCallsign is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MYCallsign. If not, see <http://www.gnu.org/licenses/>
 
 #import <UIKit/UIKit.h>
 
